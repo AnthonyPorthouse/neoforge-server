00b8eef16d25fea689e391fff0082da518b3f3c1
Anthony Porthouse <anthony@porthou.se>
2023-11-13 18:53:46 +0000

    feat: allow specifying uid and gid

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,22 @@ set -e
 
 JAR_NAME="server-${MINECRAFT_VERSION}.jar"
 
+PUID=${PUID:-1000}
+PGID=${PGID:-1000}
+USER=${USER:-"minecraft"}
+
+if [ ! "$(id -u "${USER}")" -eq "$PUID" ]; then usermod -o -u "$PUID" "${USER}" ; fi
+if [ ! "$(id -g "${USER}")" -eq "$PGID" ]; then groupmod -o -g "$PGID" "${USER}" ; fi
+
+echo "
+-----------------------------------
+GID/UID
+-----------------------------------
+User uid:    $(id -u "${USER}")
+User gid:    $(id -g "${USER}")
+-----------------------------------
+"
+
 if [ ! -f "$JAR_NAME" ]; then
     echo "Downloading Minecraft";
 
@@ -18,4 +34,9 @@ fi
 echo "Accepting EULA"
 echo "eula=true" > eula.txt
 
-java ${JAVA_OPTS} -jar "$JAR_NAME" nogui
+chown -R "${USER}":"${USER}" /minecraft
+
+
+COMMAND="${*:-"cd /minecraft; $(which java) ${JAVA_OPTS} -jar $JAR_NAME nogui"}"
+
+su -l "${USER}" -c "$COMMAND"
